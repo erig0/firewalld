erig0/firewalld

commit df4aefcbe7b7ddabfed6a44b9e0dd9eb6487fc0f

improvement(ipXtables): add utility function match sources

src/firewall/core/ipXtables.py
@@ -795,6 +795,28 @@ class ip4tables(object):
         rule += [ "-t", table, opt, interface, action, _policy ]
         return [rule]
 
+    def _rule_addr_fragment(self, opt, address, invert=False):
+        if address.startswith("ipset:"):
+            name = address[6:]
+            if opt == "-d":
+                opt = "dst"
+            else:
+                opt = "src"
+            flags = ",".join([opt] * self._fw.ipset.get_dimension(name))
+            return ["-m", "set", "--match-set", name, flags]
+        elif check_mac(address):
+            # outgoing can not be set
+            if opt == "-d":
+                raise FirewallError(INVALID_ADDR, "Can't match a destination MAC.")
+            return ["-m", "mac", "--mac-source", address.upper()]
+        else:
+            if check_single_address("ipv6", address):
+                address = normalizeIP6(address)
+            elif check_address("ipv6", address):
+                addr_split = address.split("/")
+                address = normalizeIP6(addr_split[0]) + "/" + addr_split[1]
+            return [opt, address]
+
     def build_zone_source_address_rules(self, enable, zone, policy,
                                         address, table, chain):
         add_del = { True: "-I", False: "-D" }[enable]
@@ -814,40 +836,10 @@ class ip4tables(object):
         else:
             zone_dispatch_chain = "%s_ZONES" % (chain)
 
-        action = "-g"
+        rule = [add_del, zone_dispatch_chain, "%%ZONE_SOURCE%%", zone, "-t", table]
+        rule.extend(self._rule_addr_fragment(opt, address))
+        rule.extend(["-g", _policy])
 
-        if address.startswith("ipset:"):
-            name = address[6:]
-            if opt == "-d":
-                opt = "dst"
-            else:
-                opt = "src"
-            flags = ",".join([opt] * self._fw.ipset.get_dimension(name))
-            rule = [ add_del, zone_dispatch_chain,
-                     "%%ZONE_SOURCE%%", zone,
-                     "-t", table,
-                     "-m", "set", "--match-set", name,
-                     flags, action, _policy ]
-        else:
-            if check_mac(address):
-                # outgoing can not be set
-                if opt == "-d":
-                    return ""
-                rule = [ add_del, zone_dispatch_chain,
-                         "%%ZONE_SOURCE%%", zone,
-                         "-t", table,
-                         "-m", "mac", "--mac-source", address.upper(),
-                         action, _policy ]
-            else:
-                if check_single_address("ipv6", address):
-                    address = normalizeIP6(address)
-                elif check_address("ipv6", address):
-                    addr_split = address.split("/")
-                    address = normalizeIP6(addr_split[0]) + "/" + addr_split[1]
-                rule = [ add_del, zone_dispatch_chain,
-                         "%%ZONE_SOURCE%%", zone,
-                         "-t", table,
-                         opt, address, action, _policy ]
         return [rule]
 
     def build_policy_chain_rules(self, policy, table):

commit 6f18c64b712fca0eaec69bcb7faaad1df4d72d64

fix(zone): add source with mac address
nftables supports matching the destination MAC, but iptables does not.
As such, lift the restriction from nftables. For iptables, gracefully
ignore the scenarios in which we attempt to match destination MAC.

Fixes: #703
Fixes: df4aefcbe7b7 ("improvement(ipXtables): add utility function match sources")
Fixes: 1582c5dd736a ("feat: nftables: convert to libnftables JSON interface")
Closes: #715

Co-authored-by: Eric Garver <eric@garver.life>

src/firewall/core/ipXtables.py
@@ -818,6 +818,10 @@ class ip4tables(object):
             ipv = self._fw.zone.check_source(addr)
             if ipv in ["ipv4", "ipv6"] and not self.is_ipv_supported(ipv):
                 continue
+            # iptables can not match destination MAC
+            if check_mac(addr) and chain in ["POSTROUTING", "FORWARD_OUT", "OUTPUT"]:
+                continue
+
             egress_fragments.append(self._rule_addr_fragment("-d", addr))
 
         def _generate_policy_dispatch_rule(ingress_fragment, egress_fragment):
@@ -935,6 +939,10 @@ class ip4tables(object):
             zone_dispatch_chain = "%s_ZONES_SOURCE" % (chain)
         else:
             zone_dispatch_chain = "%s_ZONES" % (chain)
+
+        # iptables can not match destination MAC
+        if check_mac(address) and chain in ["POSTROUTING", "FORWARD_OUT", "OUTPUT"]:
+            return []
 
         rule = [add_del, zone_dispatch_chain, "%%ZONE_SOURCE%%", zone, "-t", table]
         rule.extend(self._rule_addr_fragment(opt, address))
@@ -1297,6 +1305,10 @@ class ip4tables(object):
             rules.append(["-t", "filter", add_del, "%s_allow" % _policy,
                           "-o", interface, "-j", "ACCEPT"])
         else: # source
+            # iptables can not match destination MAC
+            if check_mac(source):
+                return []
+
             rules.append(["-t", "filter", add_del, "%s_allow" % _policy]
                          + self._rule_addr_fragment("-d", source) +
                          ["-j", "ACCEPT"])

src/firewall/core/nftables.py
@@ -1138,8 +1138,6 @@ class nftables(object):
             return self._set_match_fragment(address[len("ipset:"):], True if "daddr" == addr_field else False, invert)
         else:
             if check_mac(address):
-                if addr_field == "daddr":
-                    raise FirewallError(INVALID_RULE, "%s._rule_addr_fragment()", (self.__class__))
                 family = "ether"
             elif check_single_address("ipv4", address):
                 family = "ip"

src/tests/regression/gh703.at
@@ -1,0 +1,17 @@
+FWD_START_TEST([add source with mac address])
+AT_KEYWORDS(gh703)
+
+FWD_CHECK([--zone=home --add-source=34:7e:5c:3a:4c:32], 0, [ignore])
+
+NFT_LIST_RULES([inet], [nat_POSTROUTING_ZONES], 0, [dnl
+    table inet firewalld {
+        chain nat_POSTROUTING_ZONES {
+            ether daddr 34:7e:5c:3a:4c:32 goto nat_POST_home
+            goto nat_POST_public
+        }          
+    }          
+])
+
+dnl NOTE: iptables does _not_ support matching mac destination.
+
+FWD_END_TEST

src/tests/regression/regression.at
@@ -37,3 +37,4 @@ m4_include([regression/rhbz1541077.at])
 m4_include([regression/rhbz1855140.at])
 m4_include([regression/rhbz1871298.at])
 m4_include([regression/rhbz1596304.at])
+m4_include([regression/gh703.at])